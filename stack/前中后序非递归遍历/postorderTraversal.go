98dfc8db19df3f822850a252fca8badae53447a6
fateiceb <993661862@qq.com>
2021-08-27 14:04:09 +0800

    stack

---
+++ stack/前中后序非递归遍历/postorderTraversal.go
@@ -0,0 +1,16 @@
+package main
+
+import "container/list"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+func postOrder(root *TreeNode) []int {
+
+}
