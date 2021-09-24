f4f7944834e6e348476cb016c5a412d7b3e422f2
fateiceb <993661862@qq.com>
2021-09-24 21:17:36 +0800

    add 430

---
+++ backtrack/430.go
@@ -0,0 +1,35 @@
+package backtrack
+
+type Node struct {
+	Val   int
+	Prev  *Node
+	Next  *Node
+	Child *Node
+}
+
+func dfs(node *Node) (last *Node) {
+	cur := node
+	for cur != nil {
+		next := cur.Next
+		if cur.Child != nil {
+			childLast := dfs(cur.Child)
+			next = cur.Next
+			cur.Next = cur.Child
+			cur.Child.Prev = cur
+			if next != nil {
+				childLast.Next = next
+				next.Prev = childLast
+			}
+			cur.Child = nil
+			last = childLast
+		} else {
+			last = cur
+		}
+		cur = next
+	}
+	return
+}
+func flatten(root *Node) *Node {
+	dfs(root)
+	return root
+}
