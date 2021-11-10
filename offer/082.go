6c04aa93be0ab85290ab7f835277f38d460901aa
fateiceb <993661862@qq.com>
2021-11-10 22:35:11 +0800

    change

--- offer/082.go
+++ offer/082.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func combinationSum2(candidates []int, target int) (ans [][]int) {
-	path := make([]int, 0)
+	path := make([]int, 0, 16)
 	sort.Ints(candidates)
 	visit := make([]bool, len(candidates))
 	var backtrack func(idx int, remain int)
@@ -11,21 +14,20 @@ func combinationSum2(candidates []int, target int) (ans [][]int) {
 		if remain == 0 {
 			ans = append(ans, append([]int{}, path...))
 		}
-		// if idx > len(candidates) || remain < 0 {
-		//     return
-		// }
+		if idx > len(candidates) || remain < 0 {
+			return
+		}
 		for i := idx; i < len(candidates); i++ {
 			if candidates[i] > target {
 				break
 			}
-			// 剪枝
+
 			if i > 0 && !visit[i-1] && candidates[i] == candidates[i-1] {
 				continue
 			}
-
 			visit[i] = true
 			path = append(path, candidates[i])
-			backtrack(i+1, target-candidates[i])
+			backtrack(i+1, remain-candidates[i])
 			path = path[:len(path)-1]
 			visit[i] = false
 		}
